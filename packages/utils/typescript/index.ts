61f497cf7872930c0bd3ea0a8e4683bfef2f363a
D-Unit <d@unit>
2023-09-05 12:28:15 +0200

    updated ts checks, code splitting for pages

--- packages/utils/typescript/index.ts
+++ packages/utils/typescript/index.ts
@@ -1,15 +1,18 @@
 // Typescript helpers
-// Note that for deeply nested objects, typescript warnings may be a bit vague when using this utility
 
-export type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
   ? Omit<T, K>
   : never;
-export type NullUnionOmit<T, K extends string | number | symbol> = null extends T
+
+type NullUnionOmit<T, K extends string | number | symbol> = null extends T
   ? UnionOmit<NonNullable<T>, K>
   : UnionOmit<T, K>;
-export type RecursiveOmitHelper<T, K extends string | number | symbol> = {
+
+type RecursiveOmitHelper<T, K extends string | number | symbol> = {
   [P in keyof T]: RecursiveOmit<T[P], K>;
 };
+
+// Note that for deeply nested objects, typescript warnings may be a bit vague when using this utility
 export type RecursiveOmit<T, K extends string | number | symbol> = T extends {
   [P in K]: any;
 }
