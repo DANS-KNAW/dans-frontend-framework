08d00e9bbf3a5a8c25d51774e0ff7cf65e2312a0
Daan Janssen <daancjanssen@users.noreply.github.com>
2023-10-16 11:06:23 +0200

    buttons and text

--- apps/rda/src/config/pages/home.ts
+++ apps/rda/src/config/pages/home.ts
@@ -9,8 +9,8 @@ const page: Page = {
   inMenu: true,
   menuTitle: "Home",
   content: {
-    en: "<p>DANS collaborates with the OH-SMArt project to provide infrastructure for the deposit, preservation, processing and application of audiovisual materials collected by museums and research institutions. OH-SMArt concentrates on oral histories - interviews conducted at art museums.</p>",
-    nl: '<p>DANS werkt samen met het OH-SMArt project om een infrastructuur te bieden voor het indienen, verwerken, behouden en toepassen van audiovisueel materiaal dat verzameld wordt door musea en onderzoeksinstituten. OH-SMArt richt zich op mondelinge geschiedenis - interviews afgenomen bij musea.</p>',
+    en: "<p>Welcome to the RDA deposit tool. Log in and deposit your research data using our form.</p>",
+    nl: '<p>Welkom bij de RDA deposit tool. Log in en stuur je onderzoeksdata op via ons formulier.</p>',
   },
   action: {
     link: "deposit",
