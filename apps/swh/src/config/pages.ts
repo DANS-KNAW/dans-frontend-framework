d8ee40376a6198fa45afcbcf5f352c37bd788031
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2023-11-06 11:27:21 +0100

    Reworked form and removed placholder pages

--- apps/swh/src/config/pages.ts
+++ apps/swh/src/config/pages.ts
@@ -1,13 +1,9 @@
 import home from './pages/home';
 import deposit from './pages/deposit';
-import about from './pages/about';
-import support from './pages/support';
 
 const pages = [
   home,
   deposit,
-  about,
-  support,
 ];
 
 export default pages;
