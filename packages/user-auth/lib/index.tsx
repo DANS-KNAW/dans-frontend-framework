9c638de7649b757dadfcb696b6e5e9eff5373534
D-Unit <d@unit>
2023-11-24 12:09:54 +0100

    cleanup

--- packages/user-auth/lib/index.tsx
+++ packages/user-auth/lib/index.tsx
@@ -5,5 +5,5 @@ export { UserSettings } from '../src/user/UserSettings';
 export * from '../src/user/Buttons';
 export { default as i18n } from '../src/languages/i18n';
 export { fetchUserProfile } from '../src/redux/store';
-export type { Target, AuthProperty } from '../src/types';
 export { useValidateAllKeysQuery, validateKeyApi } from '../src/user/userApi';
+export type { Target, AuthProperty } from '../src/types';
