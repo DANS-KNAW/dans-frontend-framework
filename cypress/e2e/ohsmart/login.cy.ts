7b55ce48323ac1ba7d2fd5285bd9f0dc87e8455d
Michiel Zuidema <michielzuidema@gmail.com>
2024-01-31 15:36:37 +0100

    Update login test to navigate and perform file upload.

--- cypress/e2e/ohsmart/login.cy.ts
+++ cypress/e2e/ohsmart/login.cy.ts
@@ -10,7 +10,10 @@ describe('Login Test', () => {
   });
 
   it('should navigate to baseUrl and login', () => {
-    cy.visit(Cypress.config('baseUrl'));
-    cy.contains('log in', { matchCase: false }).click();
+
+    cy.contains('deposit data', { matchCase: false }).click();
+    cy.contains('files', { matchCase: false }).click();
+    // generate a 10 mb zip file in memory
+    // upload the file using the drag and drop upload function on the page 
   });
 });
