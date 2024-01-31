f11fac6a2d22d89d7a9b4cfbf4f48514dbf10f3a
Michiel Zuidema <michielzuidema@gmail.com>
2024-01-31 14:43:29 +0100

    Add login functionality to the OHSmart test suite.

--- cypress/e2e/ohsmart/login.cy.ts
+++ cypress/e2e/ohsmart/login.cy.ts
@@ -1,10 +1,14 @@
 describe('Login Test', () => {
   beforeEach(() => {
     cy.visit(Cypress.config('baseUrl'));
+    cy.contains('log in', { matchCase: false }).click();
+    // fill in user name using the username from the fixture ohsmart, the username field has id username
+    // fill in password using the password from the fixture ohsmart, the passwword field has id password
+    // click sign in button, the button has id kc-login
   });
 
   it('should navigate to baseUrl and login', () => {
-    cy.kcLogin('user');
+    // cy.visit(Cypress.config('baseUrl'));
     cy.contains('log in', { matchCase: false }).click();
   });
 });
