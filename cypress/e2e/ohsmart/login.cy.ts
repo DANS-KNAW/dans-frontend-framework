adc26936ae08d5ea57846b90b82d825bfee45f2d
Michiel Zuidema <michielzuidema@gmail.com>
2024-01-31 16:34:23 +0100

    Refactored login and file upload process in OHSmart E2E tests.

--- cypress/e2e/ohsmart/login.cy.ts
+++ cypress/e2e/ohsmart/login.cy.ts
@@ -1,28 +1,27 @@
 describe('Login Test', () => {
-  beforeEach(() => {
-    cy.visit(Cypress.config('baseUrl'));
-    cy.contains('log in', { matchCase: false }).click();
-    cy.fixture('users/ohsmart').then((data) => {
-      cy.get('#username').type(data.user_1.username);
-      cy.get('#password').type(data.user_1.password);
-      cy.get('#kc-login').click();
-    });
-  });
-
-  it('should navigate to baseUrl and login', () => {
-
-    cy.contains('deposit data', { matchCase: false }).click();
-    cy.contains('files', { matchCase: false }).click();
-    // generate a 10 mb file in memory
-    const blob = new Blob([new ArrayBuffer(1024 * 1024 * 10)], { type: 'application/zip' });
-    const file = new File([blob], 'test.zip', { type: 'application/zip' });
-
-    // using cypress-file-upload plugin attach the zip file
-    cy.get('input[type=file]').attachFile({
-      fileContent: blob.stream(),
-      fileName: 'test.zip',
-      mimeType: 'application/zip'
+    beforeEach(() => {
+        cy.visit(Cypress.config('baseUrl'));
+        cy.contains('log in', { matchCase: false }).click();
+        cy.fixture('users/ohsmart').then((data) => {
+            cy.get('#username').type(data.user_1.username);
+            cy.get('#password').type(data.user_1.password);
+            cy.get('#kc-login').click();
+        });
     });
 
-  });
+    it('should navigate to baseUrl and login', () => {
+
+        cy.contains('deposit data', { matchCase: false }).click();
+        cy.contains('files', { matchCase: false }).click();
+        // Generate a 10 MB file in memory
+        const size = 1024 * 1024 * 10; // 10 MB
+        const buffer = new ArrayBuffer(size);
+        const uint8Array = new Uint8Array(buffer);
+        for (let i = 0; i < size; i++) {
+            uint8Array[i] = 0; // or any other value to fill the file with
+        }
+
+        cy.contains('save', { matchCase: false }).click();
+
+    });
 });
