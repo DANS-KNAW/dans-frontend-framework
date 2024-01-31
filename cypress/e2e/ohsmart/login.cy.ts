5208301236a418e47cd4f2c8375c3c90d18c2224
Michiel Zuidema <michielzuidema@gmail.com>
2024-01-31 11:13:38 +0100

    Update login test to use Cypress config for base URL and add comment for case-insensitive login button search.

--- cypress/e2e/ohsmart/login.cy.ts
+++ cypress/e2e/ohsmart/login.cy.ts
@@ -1,9 +1,10 @@
 describe('Login Test', () => {
   beforeEach(() => {
-    cy.visit(Cypress.env('baseUrl'));
+    cy.visit(Cypress.config('baseUrl'));
   });
 
   it('should navigate to baseUrl and login', () => {
     cy.kcLogin('user');
+    // after requesting a token click the log in button. This button does not have an id so look for a string "log in". Make sure to be case insensitive
   });
 });
