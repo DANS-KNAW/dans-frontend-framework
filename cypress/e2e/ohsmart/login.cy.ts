b6bc19f595e25987d81de19a88679ec29977a2be
Michiel Zuidema <michielzuidema@gmail.com>
2024-01-31 10:16:06 +0100

    Create a before each method to browse to the baseUrl and obtain the URL via cypress env in the login test.

--- cypress/e2e/ohsmart/login.cy.ts
+++ cypress/e2e/ohsmart/login.cy.ts
@@ -1,6 +1,10 @@
+// create a before each method that browses to the baseUrl. You can find the url in the config file.
+// make sure to get the url via cypress env.
+
+
 describe('Login Test', () => {
   it('should navigate to baseUrl and login', () => {
+    cy.visit('https://ohsmart.dansdemo.nl/');
     cy.kcLogin('user');
-    cy.visit('/');
   });
 });
