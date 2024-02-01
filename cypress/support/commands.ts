f09aaf25475f6882ab33f41973b4cfba48b9f28d
Michiel Zuidema <michielzuidema@gmail.com>
2024-02-01 09:09:14 +0100

    Add custom command to generate large files.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,75 +1,25 @@
 /// <reference types="cypress" />
 
-import { getAuthCodeFromLocation } from "./authUtils";
+import 'cypress-file-upload';
 
-Cypress.Commands.add("kcLogin", (user: string) => {
-    Cypress.log({ name: "Login" });
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to generate a large file.
+       * @example cy.generateLargeFile('test.pdf', 100)
+       */
+      generateLargeFile(fileName: string, sizeInMB: number): Chainable<File>
+    }
+  }
+}
 
-    cy.fixture(`users/${user}`).then((userData: UserData) => {
-        const authBaseUrl = Cypress.env("auth_base_url");
-        const realm = Cypress.env("auth_realm");
-        const client_id = Cypress.env("auth_client_id");
-        const redirect_uri = Cypress.env("redirect_uri")
+Cypress.Commands.add('generateLargeFile', (fileName, sizeInMB) => {
+  // Creating a large string of specified size
+  const blob = new Blob([new Array(sizeInMB * 1024 * 1024).fill('0').join('')], { type: 'application/pdf' });
 
-        cy.request({
-            url: `${authBaseUrl}/realms/${realm}/protocol/openid-connect/auth`,
-            followRedirect: false,
-            qs: {
-                client_id,
-                redirect_uri,
-                scope: "openid",
-                response_type: "code",
-                approval_prompt: "auto",
-            },
-        })
-            .then((response) => {
-                const html = document.createElement("html");
-                html.innerHTML = response.body;
-                
-                const form = html.getElementsByTagName("form")[0];
-                console.log("dit is form", form)
-                const url = form.action;
-                console.log("dit is form action", url)
-
-                return cy.request({
-                    method: "POST",
-                    url,
-                    followRedirect: false,
-                    form: true,
-                    body: {
-                        username: userData.username,
-                        password: userData.password,
-                    },
-                });
-            })
-            .then((response) => {
-                const code = getAuthCodeFromLocation(
-                    response.headers["location"] as string,
-                );
-
-                cy.request({
-                    method: "post",
-                    url: `${authBaseUrl}/realms/${realm}/protocol/openid-connect/token`,
-                    body: {
-                        client_id,
-                        redirect_uri: redirect_uri,
-                        code,
-                        grant_type: "authorization_code",
-                    },
-                    form: true,
-                    followRedirect: false,
-                }).its("body");
-            });
-    });
+  // Using Cypress method to turn blob into a file
+  return cy.window().then(window => {
+    return new window.File([blob], fileName, { type: 'application/pdf' });
+  });
 });
-
-Cypress.Commands.add("kcLogout", () => {
-    Cypress.log({ name: "Logout" });
-    const authBaseUrl = Cypress.env("auth_base_url");
-    const realm = Cypress.env("auth_realm");
-
-    return cy.request({
-        url: `${authBaseUrl}/realms/${realm}/protocol/openid-connect/logout`,
-    });
-});
-
