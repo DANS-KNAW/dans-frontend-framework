5e9271d1f051953d7b3c22dd113f53dbb1450d46
D-Unit <d@unit>
2024-02-27 10:37:51 +0100

    updated all rda api func to pull from demo api and not hardcoded data

---
+++ packages/deposit/src/features/metadata/api/rdaApi.ts
@@ -0,0 +1,140 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import type { 
+  RdaWorkGroupResponse, 
+  RdaPathwaysResponse,
+  RdaInterestGroupsResponse,
+  RdaDomainsResponse,
+  GorcResponse
+} from "../../../types/Api";
+
+/** 
+ * TODO: This uses a dummy API. API should provide ability to search.
+ * Right now it just dumps all data.
+ */
+
+/**
+ * Recursive helper function to check if the item has a parent.
+ * Returns the title of the parent and its parents.
+ * TODO: Should be moved to API level.
+ */
+const addParentHierarchy = (data: GorcResponse[], itemId: string): string => {
+  const item = data.find(item => item.id === itemId);
+  if (!item || !item.parent_id) {
+    return '';
+  } else {
+    const parent = addParentHierarchy(data, item.parent_id);
+    return parent ? `${parent} > ${item.title}` : item.title;
+  }
+};
+
+export const rdaApi = createApi({
+  reducerPath: "rdaApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://globhut.labs.dans.knaw.nl" }),
+  endpoints: (build) => ({
+    fetchRdaDomain: build.query({
+      query: () => "domains",
+      transformResponse: (response: RdaDomainsResponse[], _meta, arg) => {
+        return response.length > 0
+          ? {
+              arg: arg,
+              response: response.map((item) => ({
+                label: item.title,
+                value: item.id,
+                idLabel: "RDA domain ID",
+                id: item.id,
+                url: item.url,
+              })),
+            }
+          : [];
+      },
+    }),
+    fetchRdaInterestGroup: build.query({
+      query: () => "interestgroups",
+      transformResponse: (response: RdaInterestGroupsResponse[], _meta, arg) => {
+        return response.length > 0
+          ? {
+              arg: arg,
+              response: response.map((item) => ({
+                label: item.title,
+                value: item.id,
+                idLabel: "RDA interest group ID",
+                id: item.id,
+                url: item.url,
+              })),
+            }
+          : [];
+      },
+    }),
+    fetchRdaPathway: build.query({
+      query: () => "pathways",
+      transformResponse: (response: RdaPathwaysResponse[], _meta, arg) => {
+        return response.length > 0
+          ? {
+              arg: arg,
+              response: response.map((item) => ({
+                label: item.title,
+                value: item.id,
+                idLabel: "RDA pathway ID",
+                id: item.id,
+                extraLabel: "Description",
+                extraContent: item.description,
+              })),
+            }
+          : [];
+      },
+    }),
+    fetchRdaWorkingGroup: build.query({
+      query: () => "workinggroups",
+      transformResponse: (response: RdaWorkGroupResponse[], _meta, arg) => {
+        return response.length > 0
+          ? {
+              arg: arg,
+              response: response.map((item) => ({
+                label: item.title,
+                value: `{"node_id":"${item.id}","UUID_WorkingGroup":"${item.parent_id}"}`,
+                idLabel: "RDA working group ID",
+                id: item.id,
+              })),
+            }
+          : [];
+      },
+    }),
+    fetchGorc: build.query({
+      query: () => "gorc",
+      transformResponse: (response: GorcResponse[]) => {
+
+        if (response.length > 0) {
+          const updatedResponse = response.map(item => ({
+            ...item,
+            parent_hierarchy: addParentHierarchy(response, item.id)
+          }));
+
+          return {
+            response: updatedResponse.map((item) => ({
+              label: item.title,
+              value: item.id,
+              extraLabel: "description",
+              extraContent: item.description,
+              idLabel: "GORC ID",
+              id: item.id,
+              categoryLabel: "Hierarchy",
+              categoryContent: item.parent_hierarchy,
+            })),
+          };
+
+        }
+        else {
+          return [];
+        }
+      },
+    }),
+  }),
+});
+
+export const { 
+  useFetchRdaDomainQuery,
+  useFetchRdaInterestGroupQuery,
+  useFetchRdaPathwayQuery,
+  useFetchRdaWorkingGroupQuery,
+  useFetchGorcQuery,
+} = rdaApi;
