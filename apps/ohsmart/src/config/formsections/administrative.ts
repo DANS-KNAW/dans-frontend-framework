32d0796e621ed232eb2baa6d93c0878cc48e0dac
D-Unit <d@unit>
2024-04-15 11:07:42 +0200

    ohsmart config updates per open issues doc

--- apps/ohsmart/src/config/formsections/administrative.ts
+++ apps/ohsmart/src/config/formsections/administrative.ts
@@ -35,51 +35,7 @@ const section: InitialSectionType = {
         en: "In case of an embargo, you should provide a future date for this field.",
         nl: "In het geval van een embargo kun je hier een toekomstige datum invullen.",
       },
-    },
-    {
-      type: "group",
-      label: {
-        en: "Contact information",
-        nl: "Contactinformatie",
-      },
-      name: "contact",
-      description: {
-        en: "The person to contact regarding the metadata (curator)",
-        nl: "Contactpersoon voor de metadata (curator)",
-      },
-      fields: [
-        {
-          type: "text",
-          label: {
-            en: "Name",
-            nl: "Naam",
-          },
-          name: "contact_name",
-          autofill: "name",
-          disabled: true,
-        },
-        {
-          type: "text",
-          label: {
-            en: "Affiliation",
-            nl: "Affiliatie",
-          },
-          name: "contact_affiliation",
-          autofill: "voperson_external_affiliation",
-          disabled: true,
-        },
-        {
-          type: "text",
-          label: {
-            en: "Email",
-            nl: "Email",
-          },
-          name: "contact_email",
-          autofill: "email",
-          disabled: true,
-        },
-      ],
-    },
+    }
   ],
 };
 
