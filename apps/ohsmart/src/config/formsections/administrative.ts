6c9fae1870dcc1602e3c147b7d117789ff83cad9
D-Unit <d@unit>
2024-06-05 15:01:00 +0200

    added metadata language field to ohsmart

--- apps/ohsmart/src/config/formsections/administrative.ts
+++ apps/ohsmart/src/config/formsections/administrative.ts
@@ -11,14 +11,29 @@ const section: InitialSectionType = {
     {
       type: "autocomplete",
       label: {
-        en: "Language",
-        nl: "Taal",
+        en: "Language of interview",
+        nl: "Taal van interview",
       },
-      name: "language",
+      name: "language_interview",
       required: true,
       description: {
-        en: "Language of interview",
-        nl: "Taal van het interview",
+        en: "Provide the language the interview was held in",
+        nl: "Geef de taal waarin het interview is gehouden op",
+      },
+      options: languageList,
+    },
+    {
+      type: "autocomplete",
+      label: {
+        en: "Language of metadata",
+        nl: "Taal van metadata",
+      },
+      name: "language_metadata",
+      required: true,
+      multiselect: true,
+      description: {
+        en: "Provide the language(s) of the metadata you've entered, e.g. the description and title",
+        nl: "Geef de taal of talen op van deze metadata, denk bijvoorbeeld aan titel en bescrijving",
       },
       options: languageList,
     },
