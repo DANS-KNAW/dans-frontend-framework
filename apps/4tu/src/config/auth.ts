c2fcc13abf5d9781862c0451bee2a405e341d316
D-Unit <d@unit>
2024-06-07 08:49:03 +0200

    dummy data

--- apps/4tu/src/config/auth.ts
+++ apps/4tu/src/config/auth.ts
@@ -2,7 +2,7 @@ const authProvider = {
   authority: import.meta.env.VITE_OIDC_AUTHORITY as string,
   client_id: import.meta.env.VITE_OIDC_CLIENT_ID as string,
   scope: import.meta.env.VITE_OIDC_SCOPE as string,
-  redirect_uri: encodeURIComponent(`${window.location.origin}/signin-callback/${window.location.search}`),
+  redirect_uri: encodeURIComponent(`${window.location.origin}/signin-callback`),
   loadUserInfo: true,
 };
 
--- apps/4tu/src/config/formsections/main.ts
+++ apps/4tu/src/config/formsections/main.ts
@@ -1,11 +1,31 @@
 import type { InitialSectionType } from "@dans-framework/deposit";
+import LZString from 'lz-string';
+
+const fakeData = LZString.compressToEncodedURIComponent(JSON.stringify({
+  "title": "This is a title",
+  "affiliation": {
+    "label": "Deepam Educational Society for Health",
+    "value": "https://ror.org/00nyy7p10"
+  },
+  "research_domain": {
+    "id": "D12700",
+    "label": "Gases, fluid dynamics, plasma physics",
+    "value": "https://www.narcis.nl/classification/D12700"
+  },
+  "datastation": "ssh",
+  "etc": "etc"
+}));
+
+console.log(fakeData)
 
 const uri = new URL(document.location.toString());
 const searchParams = uri.searchParams.get("data");
 const json = 
   searchParams ?
-  JSON.parse(atob(searchParams as string)) :
+  JSON.parse(LZString.decompressFromEncodedURIComponent(searchParams as string)) :
   null;
+
+console.log(json)
 
 const section: InitialSectionType = {
   id: "data",
