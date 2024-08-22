10d7a533acf762a06d3b0d228df395e833d71d64
D-Unit <d@unit>
2024-08-22 13:38:16 +0200

    updates

--- packages/deposit/src/features/metadata/fields/Map.tsx
+++ packages/deposit/src/features/metadata/fields/Map.tsx
@@ -6,7 +6,7 @@ import TextField from "@mui/material/TextField";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import InputAdornment from "@mui/material/InputAdornment";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { StatusIcon } from "../../generic/Icons";
 import { setField } from "../metadataSlice";
@@ -20,6 +20,7 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
 import './Map.css';
 
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
 import List from '@mui/material/List';
 import ListSubheader from '@mui/material/ListSubheader';
 import ListItem from '@mui/material/ListItem';
@@ -30,6 +31,12 @@ import PolylineIcon from '@mui/icons-material/Polyline';
 import PlaceIcon from '@mui/icons-material/Place';
 import HighlightAltIcon from '@mui/icons-material/HighlightAlt';
 import PentagonIcon from '@mui/icons-material/Pentagon';
+import { useFetchGeonamesFreeTextQuery } from "../api/geonames";
+import type { QueryReturnType } from "../../../types/Api";
+import CircularProgress from "@mui/material/CircularProgress";
+import Autocomplete from "@mui/material/Autocomplete";
+import Typography from "@mui/material/Typography";
+import { useDebounce } from "use-debounce";
 
 /** 
  * Map field
@@ -47,52 +54,38 @@ const DrawMap = ({
   const status = getFieldStatus(field);
   const { t, i18n } = useTranslation("metadata");
   const formDisabled = useAppSelector(getFormDisabled);
+  const [geonamesValue, setGeonamesValue] = useState<string>("");
+  const [viewState, setViewState] = useState({
+    longitude: 4.342779,
+    latitude: 52.080738,
+    zoom: 8,
+  });
+
+  // move map to selected GeoNames value
+  useEffect(() => {
+    geonamesValue && setViewState({
+      longitude: geonamesValue.coordinates[1],
+      latitude: geonamesValue.coordinates[0],
+      zoom: 10,
+    });
+  }, [geonamesValue])
 
   return (
     <Box>
-      <Stack direction="row" alignItems="center">
-        <TextField
-          fullWidth
-          error={status === "error" && field.touched}
-          helperText={status === "error" && field.touched && t("incorrect")}
-          variant="outlined"
-          label={lookupLanguageString(field.label, i18n.language)}
-          required={field.required}
-          value={field.value || ""}
-          disabled={field.disabled || formDisabled}
-          onChange={(e) =>
-            dispatch(
-              setField({
-                sectionIndex: sectionIndex,
-                id: field.id,
-                value: e.target.value,
-              }),
-            )
-          }
-          placeholder={field.placeholder}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <StatusIcon
-                  status={status}
-                  title={lookupLanguageString(field.description, i18n.language)}
-                />
-              </InputAdornment>
-            ),
-          }}
-          inputProps={{ "data-testid": `${field.name}-${field.id}` }}
-        />
-      </Stack>
+      <GeonamesApiField 
+        value={geonamesValue}
+        setValue={setGeonamesValue}
+        disabled={formDisabled || field.disabled}
+        label={lookupLanguageString(field.label, i18n.language)}
+      />
       <Map
-
-        initialViewState={{
-          longitude: 4.342779,
-          latitude: 52.080738,
-          zoom: 8
-        }}
+        {...viewState}
+        onMove={(e) => setViewState(e.viewState)}
         style={{
           width: '100%', 
           height: 400,
+          borderRadius: '5px',
+          border: "1px solid rgba(0,0,0,0.23)"
         }}
         mapStyle={`https://api.maptiler.com/maps/landscape/style.json?key=${import.meta.env.VITE_MAPTILER_API_KEY}`}
       >
@@ -111,6 +104,8 @@ const controls = ["simple_select", "draw_point", "draw_line_string", "draw_polyg
 const DrawControls = () => {
   const { t } = useTranslation("map");
   const [ selectedMode, setSelectedMode ] = useState(controls[0]);
+
+  // move this to redux store
   const [ features, setFeatures ] = useState({});
 
   console.log(features)
@@ -148,8 +143,18 @@ const DrawControls = () => {
     >
       <List
         subheader={
-          <ListSubheader component="div">
+          <ListSubheader component="div" sx={{ display: 'flex' , alignItems: 'center'}}>
             {t('draw')}
+            <Tooltip title={
+              <Trans
+                i18nKey={`map:drawExplanation`}
+                components={[
+                  <Typography gutterBottom variant="body2" />,
+                ]}
+              />
+            }>
+              <InfoRoundedIcon color="neutral" fontSize="small" sx={{ml: 1}} />
+            </Tooltip>
           </ListSubheader>
       }>
         {controls.map(control =>
@@ -158,7 +163,7 @@ const DrawControls = () => {
               onClick={() => setSelectedMode(control)}
               selected={control === selectedMode}
             >
-              <ListItemIcon>
+              <ListItemIcon sx={{minWidth: 40}}>
                 { 
                     control === "simple_select" 
                   ? <HighlightAltIcon />
@@ -216,3 +221,64 @@ const DrawControl = ({
 
   return null;
 }
+
+const GeonamesApiField = ({
+  value,
+  setValue,
+  disabled,
+  label
+}) => {
+  const { t } = useTranslation("metadata");
+  const [inputValue, setInputValue] = useState<string>("");
+  const debouncedInputValue = useDebounce(inputValue, 500)[0];
+  // Fetch data on input change
+  const { data, isFetching, isLoading } =
+    useFetchGeonamesFreeTextQuery<QueryReturnType>(debouncedInputValue, {
+      skip: debouncedInputValue === "",
+    });
+
+  return (
+    <Autocomplete
+      fullWidth
+      includeInputInList
+      options={
+        (
+          inputValue &&
+          debouncedInputValue === inputValue &&
+          data &&
+          data.arg === debouncedInputValue
+        ) ?
+          data.response
+        : []
+      }
+      value={value || null}
+      inputValue={inputValue || value?.label as string || ""}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={label}
+        />
+      )}
+      onChange={(_e, newValue, _reason) => setValue(newValue as OptionsType)}
+      filterOptions={(x) => x}
+      onInputChange={(e, newValue) => {
+        e && e.type === "change" && setInputValue(newValue);
+        e && (e.type === "click" || e.type === "blur") && setInputValue("");
+      }}
+      noOptionsText={!inputValue ? t("startTyping", {api: t("geonames")}) : t("noResults")}
+      loading={isLoading || isFetching || inputValue !== debouncedInputValue}
+      loadingText={
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="end"
+        >
+          {t("loading")} <CircularProgress size={18} />
+        </Stack>
+      }
+      forcePopupIcon
+      clearOnBlur
+      disabled={disabled}
+    />
+  );
+};
