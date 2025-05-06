234f2c061f6520ce705824a52d9992ef6df13318
D-Unit <d@unit>
2025-05-06 11:04:24 +0200

    check for deleted target dataset

--- packages/user-auth/src/user/UserSubmissions.tsx
+++ packages/user-auth/src/user/UserSubmissions.tsx
@@ -213,7 +213,7 @@ const SubmissionList = ({
                     );
                     navigate(`/${depositSlug}`);
                   }}
-                  disabled={params.processing || params.row.legacy}
+                  disabled={params.processing || params.row.legacy || params.row.remoteDeleted}
                 />
               </Tooltip>
             ),
@@ -347,7 +347,8 @@ const SubmissionList = ({
                 {params.value.map((v: TargetOutput, i: number) => (
                   <SingleTargetStatus
                     target={v}
-                    depositStatus={depositStatus}
+                    remoteChanges={params.row.remoteChanges}
+                    remoteDeleted={params.row.remoteDeleted}
                     key={i}
                   />
                 ))}
@@ -375,7 +376,8 @@ const SubmissionList = ({
       id: d["dataset-id"],
       created: type === "published" ? d["submitted-at"] : d["saved-at"],
       title: d["title"],
-      remoteChanges: d["targets"].some(t => t.diff && t.diff.hasOwnProperty("data")),
+      remoteChanges: d["targets"].some(t => t.diff && Object.keys(t.diff).length > 0),
+      remoteDeleted: d["targets"].some(t => t.diff && Object.values(t.diff).includes(404)),
       legacy: d["legacy-form"] || d["acp-version"] === "unknown",
       ...(type === "published" ? { status: d["targets"] } : null),
     }));
@@ -453,11 +455,13 @@ const MotionGridRow = motion(ForwardRow);
 
 // A separate component for a target, needs to have it's own state to display popover
 const SingleTargetStatus = ({
-  depositStatus,
   target,
+  remoteChanges,
+  remoteDeleted,
 }: {
-  depositStatus: DepositStatus;
   target: TargetOutput;
+  remoteChanges: boolean;
+  remoteDeleted: boolean;
 }) => {
   const { t } = useTranslation("user");
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
@@ -473,8 +477,8 @@ const SingleTargetStatus = ({
   return (
     <>
       <Stack direction="row" alignItems="center" pt={0.1} pb={0.1} spacing={0.5}>
-        {target.diff && target.diff.hasOwnProperty("data") && (
-          <Tooltip title={t("remoteChanges")} placement="left">
+        {remoteChanges && (
+          <Tooltip title={t(remoteDeleted ? "remoteDeleted" : "remoteChanges")} placement="left">
             <ErrorOutlineIcon fontSize="small" color="warning" />
           </Tooltip>
         )}
