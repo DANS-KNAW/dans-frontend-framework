b5cf379dee18db94cb03cd14ef13d29904596083
DJ <daan.janssen@dans.knaw.nl>
2025-07-30 09:12:32 +0200

    setup new app for fair assessment ui

---
+++ apps/fair-assessment/src/config/pages/assessment.ts
@@ -0,0 +1,15 @@
+import type { Page } from "@dans-framework/pages";
+
+const page: Page = {
+  id: "assessments",
+  name: "Assessments",
+  slug: "assessments",
+  template: "assessments",
+  inMenu: true,
+  menuTitle: {
+    en: "Assessments",
+    nl: "Beoordelingen",
+  },
+};
+
+export default page;
